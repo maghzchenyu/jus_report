e31e783c090a3f93dcc8f3fcc2e0e5d19c821da2
chenyu <4325548@163.com>
2025-01-10 18:52:14 +0800

    结构优化

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -16,7 +16,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final JusReport plugin = JusReport();
+    final JSReport plugin = JSReport();
     // The version string depends on the host platform running the test, so
     // just assert that some non-empty string is returned.
   });
--- lib/jus_report.dart
+++ lib/jus_report.dart
@@ -11,96 +11,71 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sim_plugin/sim_plugin.dart';
 import 'package:uuid/uuid.dart';
 
-class JusReport {
-  static JusReport? _instance;
+class JSReport {
+  static JSReport? _instance;
   static const _androidIdPlugin = AndroidId();
   final _publicData = _ReportPublicData();
   AliyunLogDartSdk? _aliyunLogSdk;
   late final AppLifecycleListener _appLifecycleListener;
+  final _userProperty = <String, dynamic>{};
+  var _isDebug = false;
 
-  factory JusReport() {
-    _instance ??= JusReport._internal();
+  factory JSReport() {
+    _instance ??= JSReport._internal();
     return _instance!;
   }
 
-  JusReport._internal() {}
+  JSReport._internal() {}
 
   /// 初始化
+  /// [isDebug] 是否开启日志
   /// [endpoint] 日志服务地址
   /// [project] 日志服务项目
   /// [logstore] 日志服务日志库
   /// [accessKeyId] 日志服务AccessKeyId
   /// [accessKeySecret] 日志服务AccessKeySecret
-  setupJusReport(
-      {required String endpoint,
+  setup(
+      {bool? isDebug,
+      required String endpoint,
       required String project,
       required String logstore,
       required String accessKeyId,
       required String accessKeySecret}) async {
+    if (isDebug != null) {
+      _isDebug = isDebug;
+    }
     await _setupAliLogSDK(
         endpoint, project, logstore, accessKeyId, accessKeySecret);
     await _initGlobalReportData();
     _dynamicListen();
   }
 
-  /// 设置账号相关数据
-  /// [googleID] 谷歌ID
-  /// [userID] 用户ID
-  /// [premiumStatus] 会员订阅状态
-  /// [creditInventory] 积分
-  setAccountPublicData(
-      {String? googleID,
-      String? userID,
-      int? premiumStatus,
-      int? creditInventory}) {
-    if (googleID != null) {
-      _publicData.googleID = googleID;
-    }
-    if (userID != null) {
-      _publicData.userID = userID;
-    }
-    if (premiumStatus != null) {
-      _publicData.premiumStatus = premiumStatus;
-    }
-    if (creditInventory != null) {
-      _publicData.creditInventory = creditInventory;
-    }
+  /// 设置用户ID
+  /// [userId] 用户ID
+  setUserId(String userId) {
+    _publicData.userID = userId;
   }
 
-  /// 设置公共数据
-  /// [systemLang] 系统语言
-  /// [countryCode] 国家代码
-  /// [platID] 平台ID
-  setCommonPublicData({
-    String? systemLang,
-    String? countryCode,
-    String? platID,
-    String? deviceID,
+  /// 设置用户全局属性
+  /// [name] 属性名
+  /// [value] 属性值
+  setUserProperty({
+    required String name,
+    required String? value,
   }) {
-    if (systemLang != null) {
-      _publicData.systemLang = systemLang;
-    }
-    if (countryCode != null) {
-      _publicData.countryCode = countryCode;
-    }
-    if (platID != null) {
-      _publicData.platID = platID;
-    }
-    if (deviceID!= null) {
-      _publicData.deviceID = deviceID;
-    }
+    _userProperty[name] = value;
   }
 
-  /// 清除账号相关数据
-  clearAccountPublicData() {
-    _publicData.googleID = null;
-    _publicData.userID = null;
+  /// 移除用户全局属性
+  /// [name] 属性名
+  removeUserProperty(String name) {
+    _userProperty.remove(name);
   }
 
   /// 初始化埋点全局数据
   _initGlobalReportData() async {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    _publicData.progressID = _getUUID();
+    _publicData.processID = _getUUID();
     _publicData.sessionID = _getUUID();
     if (Platform.isAndroid) {
       _publicData.androidID = await _androidIdPlugin.getId();
@@ -109,7 +84,7 @@ class JusReport {
     }
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     _publicData.appName = packageInfo.appName;
-    _publicData.appPackage = packageInfo.packageName;
+    _publicData.packageName = packageInfo.packageName;
     _publicData.clientVersion = packageInfo.version;
     if (Platform.isAndroid) {
       _publicData.systemType = "Android";
@@ -153,7 +128,6 @@ class JusReport {
         .listen((NetworkStatus networkStatus) {
       _publicData.network = _getNetworkStatusName(networkStatus);
     });
-
     _appLifecycleListener = AppLifecycleListener(
       onHide: _onHide,
     );
@@ -172,12 +146,14 @@ class JusReport {
     configuration.accessKeyId = accessKeyId;
     configuration.accessKeySecret = accessKeySecret;
     configuration.persistent = true;
-    Directory directory = await getApplicationDocumentsDirectory();
+    Directory directory = await getApplicationSupportDirectory();
     configuration.persistentFilePath = directory.path;
     configuration.persistentMaxFileCount = 20;
     _aliyunLogSdk = AliyunLogDartSdk();
     LogProducerResult result = await _aliyunLogSdk!.initProducer(configuration);
-    print('init aliyun log sdk result: $result');
+    if (_isDebug) {
+      print('aliyun log init result: ${result.toString()}');
+    }
   }
 
   /// 生成UUID
@@ -190,19 +166,26 @@ class JusReport {
   /// 上报事件
   /// [eventID] 事件ID
   /// [reportData] 上报数据
-  reportEvent(String eventID, Map<String, dynamic> reportData) async {
+  logEvent(String eventID, Map<String, dynamic> reportData) async {
     Map<String, dynamic> reportMap = _publicData.toJson();
     reportData.forEach((key, value) {
       reportMap[key] = value;
     });
+    _userProperty.forEach((key, value) {
+      reportMap[key] = value;
+    });
+    DateTime dateTime = DateTime.now();
     reportMap[_ReportJsonKey.requestID.name] = _getUUID();
     reportMap[_ReportJsonKey.eventID.name] = eventID;
-    reportMap[_ReportJsonKey.eventTime.name] = DateTime.now().toString();
+    reportMap[_ReportJsonKey.eventTime.name] = dateTime.toString();
     reportMap[_ReportJsonKey.eventTimeStamp.name] =
-        DateTime.now().millisecondsSinceEpoch;
-    reportMap[_ReportJsonKey.timeZone.name] = DateTime.now().timeZoneName;
+        dateTime.millisecondsSinceEpoch;
+    reportMap[_ReportJsonKey.timeZone.name] = dateTime.timeZoneName;
     LogProducerResult code = await _aliyunLogSdk!.addLog(reportMap);
-    print('report event json: ${reportMap.toString()}');
+    if (_isDebug) {
+      print('aliyun log add log data: ${code.toString()}');
+      print('aliyun log add log result code: $code');
+    }
   }
 
   /// 获取网络状态名称
@@ -228,17 +211,18 @@ class JusReport {
 }
 
 class _ReportPublicData {
-  String? progressID;
+  String? userID;
+
+  String? processID;
+  String? androidID;
+  String? proID;
   String? sessionID;
   String? IDFA;
   String? IDFV;
-  String? androidID;
-  String? googleID;
   String? firebaseID;
   String? appName;
   String? platID;
-  String? userID;
-  String? appPackage;
+  String? packageName;
   String? clientVersion;
   String? telecomOper;
   String? systemType;
@@ -250,22 +234,19 @@ class _ReportPublicData {
   bool? isEmulator;
   String? deviceID;
   String? countryCode;
-  int? premiumStatus;
-  int? creditInventory;
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> map = {};
-    map[_ReportJsonKey.progressID.name] = progressID;
+    map[_ReportJsonKey.processID.name] = processID;
     map[_ReportJsonKey.sessionID.name] = sessionID;
     map[_ReportJsonKey.IDFA.name] = IDFA;
     map[_ReportJsonKey.IDFV.name] = IDFV;
     map[_ReportJsonKey.androidID.name] = androidID;
-    map[_ReportJsonKey.googleID.name] = googleID;
     map[_ReportJsonKey.firebaseID.name] = firebaseID;
     map[_ReportJsonKey.appName.name] = appName;
     map[_ReportJsonKey.platID.name] = platID;
     map[_ReportJsonKey.userID.name] = userID;
-    map[_ReportJsonKey.appPackage.name] = appPackage;
+    map[_ReportJsonKey.packageName.name] = packageName;
     map[_ReportJsonKey.clientVersion.name] = clientVersion;
     map[_ReportJsonKey.telecomOper.name] = telecomOper;
     map[_ReportJsonKey.systemType.name] = systemType;
@@ -276,8 +257,6 @@ class _ReportPublicData {
     map[_ReportJsonKey.isEmulator.name] = isEmulator;
     map[_ReportJsonKey.deviceID.name] = deviceID;
     map[_ReportJsonKey.countryCode.name] = countryCode;
-    map[_ReportJsonKey.premiumStatus.name] = premiumStatus;
-    map[_ReportJsonKey.creditInventory.name] = creditInventory;
     return map;
   }
 }
@@ -286,7 +265,7 @@ enum _ReportJsonKey {
   requestID,
   eventID,
   systemDeviceType,
-  progressID,
+  processID,
   sessionID,
   eventTime,
   eventTimeStamp,
@@ -295,23 +274,17 @@ enum _ReportJsonKey {
   IDFA,
   IDFV,
   androidID,
-  googleID,
   firebaseID,
-  domainID,
   platID,
   userID,
-  premiumStatus,
-  appPlatfrom,
-  appPackage,
+  packageName,
   countryCode,
   clientVersion,
   telecomOper,
   systemType,
   network,
-  proxy,
   systemLang,
   isEmulator,
   appName,
   systemVersion,
-  creditInventory
 }
